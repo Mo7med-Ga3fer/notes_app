7ab73c21d94f1e5ad1e5f38d959f0eedfd2253a2
Mosala72003 <mosala72030@gmail.com>
2023-07-24 20:43:00 +0300

    change date theme

--- lib/widgets/add_note_form.dart
+++ lib/widgets/add_note_form.dart
@@ -59,7 +59,7 @@ class _AddNoteFormState extends State<AddNoteForm> {
                 if (formKey.currentState!.validate()) {
                   formKey.currentState!.save();
                   var dateForNow = DateTime.now();
-                  var formattedDate = DateFormat.yMEd().format(dateForNow);
+                  var formattedDate = DateFormat.yMd().format(dateForNow);
                   var noteModel = NoteModel(
                       title: title!,
                       subTitle: subtitle!,
